ab0e521f90115038935729c6f24c4f63c5b5d53b
MLZ6 <kopomasuga64@gmail.com>
2024-07-10 00:06:51 +0200

    Added Axios & display Paris temperature

---
+++ src/Weather.js
@@ -0,0 +1,15 @@
+import React from "react";
+import axios from "axios";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(
+      `The weather in ${response.data.name} is ${response.data.main.temp}°C`
+    );
+  }
+  let apiKey = "25fad9f7e87157d33dde0f82ab269ee8";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metrric`;
+
+  axios.get(apiUrl).then(handleResponse);
+  return <h2>Hello World</h2>;
+}
