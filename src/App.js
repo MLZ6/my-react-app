0f0032e396222a27e80ddb2a3e5f8078c52f02dd
MLZ6 <kopomasuga64@gmail.com>
2024-07-10 04:17:44 +0200

    Added footer

--- src/App.js
+++ src/App.js
@@ -7,9 +7,31 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello to my World!!</h1>
         <br />
-        <Weather city="Paris" />
+        <Weather />
+        <br />
+        <footer>
+          Coded by{" "}
+          <a href="https://github.com/MLZ6" target="_blank" rel="noreferrer">
+            Maitumelo Moaletsi
+          </a>{" "}
+          is{" "}
+          <a
+            href="https://github.com/MLZ6/my-react-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-sourced on Github
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://monumental-dasik-fdb48b.netlify.app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
       </header>
     </div>
   );
